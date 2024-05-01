48688baf1af3256235a053272f1cdeb23bba6dbb
sojundan <164093284+sojundan@users.noreply.github.com>
2024-05-01 00:29:11 +0000

    changes to java

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const firebaseConfig = {
     appId: "1:177571392849:web:ce950af4e60a15f736b770",
     measurementId: "G-JZC2N6T4GG"
 };
+
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 console.log("firebase setup complete!");
@@ -18,3 +19,58 @@ function buttonclicked() {
   const number = Math.floor(Math.random() * affirmations.length);
   alert(affirmations[number]);
 };
+
+function createForm() {
+  console.log("createForm called");
+  var First_Name = document.getElementById("First_Name").value;
+  var Last_Name = document.getElementById("Last_Name").value;
+  var Period1_2 = document.getElementById("1_2").value;
+  var Period3_4 = document.getElementById("3_4").value;
+  var Period8_9 = document.getElementById("8_9").value;
+  var Email = document.getElementById("Email").value;
+  var call_yes = document.getElementById("reach_out_yes").value;
+  var call_no = document.getElementById("reach_out_no").value;
+  var seat = document.getElementById("seat").value;
+  var HW = document.getElementById("HW").value;
+  var too_fast = document.getElementById("too_fast").value;
+  var quiz = document.getElementById("quiz").value;
+  var test = document.getElementById("test").value;
+  var other = document.getElementById("other").value;
+  var Comments = document.getElementById("info").value;
+  var N2K = document.getElementById("comments").value;
+
+var formData = {
+  timestamp: Date.now(),
+  form: form,
+  First_Name: First_Name,
+  Last_Name: Last_Name,
+  1_2: Period1_2,
+  3_4: Period3_4,
+  8_9: Period8_9,
+  Email: Email,
+  reach_out_yes: call_yes,
+  reach_out_no: call_no,
+  seat: seat,
+  HW: HW,
+  too_fast: too_fast,
+  quiz: quiz,
+  test: test,
+  other: other,
+  info: Comments,
+  comments: N2K,
+};
+
+var formJSON = JSON.stringify(formData);
+console.log("formJSON: " + formJSON);
+  db.collection("forms").doc(formData.timestamp).set(formData);
+return formData;
+};
+
+function saveForm() {
+  console.log("saveForm() called");
+
+  var formData = createForm();
+  db.collection("forms").doc(formData.timestamp).set(formData);
+  alert("Thank you for your submission!");
+}
+
